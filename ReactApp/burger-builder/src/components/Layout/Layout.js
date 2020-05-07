c7810d4817cdae2974bba1224958a4507c2f9bcb
kpawank <kpawank@mindfiresolutons.com>
2020-05-07 22:43:05 +0530

    Created a basic version of MyBurger and added toolbar and SideDrawer

--- ReactApp/burger-builder/src/components/Layout/Layout.js
+++ ReactApp/burger-builder/src/components/Layout/Layout.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
+        <SideDrawer />
         {/* in this i will o/p the component we wrap with this layout so props argument accepted */}
         <main className={classes.Content}>
             {props.children}
