ba1f286b48933e716c93e3d7a3e065943f57d7e8
kpawank <kpawank@mindfiresolutons.com>
2020-05-14 23:42:35 +0530

    Successfully added advanced redux to my project

--- ReactApp/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ ReactApp/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,8 +8,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHanlder/withErrorHandler';
+import * as actions from '../../store/actions/index';
 import axios from '../../axios-orders';
-import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component{
     // constructor(props){
@@ -20,19 +20,11 @@ class BurgerBuilder extends Component{
     //alternate of above syntax as it is shorthand notation
     state = {
         purchasing: false,
-        loading: false,
-        error: false
     }
 
     componentDidMount (){
         console.log(this.props);
-        // axios.get('https://react-my-burger-1ed6b.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({ingredients: response.data});
-        //     })
-        //     .catch(error => {
-        //         this.setState({error: true});
-        //     });
+        this.props.onInitIngredients();
     }
 
     updatePurchaseState(ingredients){
@@ -56,6 +48,7 @@ class BurgerBuilder extends Component{
 
     // when u click contunue 
     purchaseContinueHandler = () => {
+        this.props.onInitPurchase();
         this.props.history.push('/checkout');
     }
 
@@ -69,7 +62,7 @@ class BurgerBuilder extends Component{
         //disabledInfo:{salad: true,meat: false....}
         let orderSummary = null;
 
-        let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
+        let burger = this.props.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
         if(this.props.ings){
             burger =(   
                 <Aux>
@@ -90,10 +83,7 @@ class BurgerBuilder extends Component{
                 ingredients={this.props.ings}
             />;
         }
-        if( this.state.loading){
-            orderSummary = <Spinner />;
-        }
-
+        //{salad: true, meat: false,...}
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
@@ -107,15 +97,18 @@ class BurgerBuilder extends Component{
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        error: state.burgerBuilder.error,
     };
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName}),
+        onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(actions.initIngredients()),
+        onInitPurchase: () => dispatch(actions.purchaseInit()),
     };
 }
 
--- ReactApp/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ ReactApp/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHanlder/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component{
     state = {
@@ -91,13 +93,11 @@ class ContactData extends Component{
             }
         },
         formIsValid: false,
-        loading: false
     }
 
     orderHandler = (event) => {
         event.preventDefault();
 
-        this.setState({loading: true});
         const formData = {};
         for (let formElementIdentifier in this.state.orderForm){
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
@@ -107,17 +107,8 @@ class ContactData extends Component{
             price: this.props.price,
             orderData: formData
         }
-        axios.post('/orders.json',order)
-            .then(response => {
-                //console.log(response);
-                this.setState({loading: false});
-                //successful entry of data in firebase then redirect to the root page
-                this.props.history.push('/');
-            })
-            .catch(error => {
-                //console.log(error);
-                this.setState({loading: false});
-            });
+
+        this.props.onOrderBurger(order);
     }
 
     checkValidity(value,rules){
@@ -189,7 +180,7 @@ class ContactData extends Component{
                 >ORDER</Button>
             </form>
         );
-        if(this.state.loading){
+        if(this.props.loading){
             form = <Spinner />;
         }
         return(
@@ -203,9 +194,16 @@ class ContactData extends Component{
 
 const mapStateToProps = state => {
     return{
-        ings: state.ingredients,
-        price: state.totalPrice
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     };
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+    return{
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(ContactData, axios));
